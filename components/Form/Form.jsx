82b4e376c626ac963586bf089d987b8b18e3c134
hossein-heydari <hosseinheydari790228@gmail.com>
2021-09-17 20:39:14 +0430

    add from component

---
+++ components/Form/Form.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Form.module.css";
+
+export const Form = () => {
+  function submit(e) {
+    e.preventDefault();
+    alert("Will be sent!");
+  }
+
+  return (
+    <form>
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" />
+
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" />
+
+      <label htmlFor="Address">Address</label>
+      <input type="text" id="Address" />
+
+      <input
+        type="button"
+        className={styles.btn}
+        onClick={submit}
+        value="Checkout"
+      />
+    </form>
+  );
+};
