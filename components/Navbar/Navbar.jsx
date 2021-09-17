e3283e16815c59c96e826c783dbb5bbf219eb7c1
hossein-heydari <hosseinheydari790228@gmail.com>
2021-09-17 20:44:32 +0430

    add navbar component

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+export const Navbar = ({ filter, setfilter, len }) => {
+  function change(e) {
+    setfilter({ ...filter, [e.target.name]: e.target.value });
+  }
+
+  return (
+    <div className={styles.menu}>
+      <div>{len} Products</div>
+      <div>
+        <span>Order </span>
+        <select onChange={change} value={filter.price} name="price" id="">
+          <option value="lowest">Lowest</option>
+          <option value="highes">Highest</option>
+        </select>
+      </div>
+      <div>
+        <span>Filter </span>
+        <select name="size" onChange={change} value={filter.size} id="">
+          <option value="ALL">All</option>
+          <option value="XS">XS</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+          <option value="XXL">XXL</option>
+        </select>
+      </div>
+    </div>
+  );
+};
